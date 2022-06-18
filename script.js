80103ebd7b90d8737ef41418ff72390e06d5161f
Abdirahman Ali <abdirahman.ali@abdirah-ltm4tx2.internal.salesforce.com>
2022-06-18 13:01:57 -0700

    prog 3

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // The constants 
 let page_num =1;
+// The search input
+let search_movie = '';
 //API key and all the urls being used in the project, I'm thinking of a different route then hard coding.
 const apiKey= "be8ae1c7519989b52d8ecbf396d76b45";
 const now_playing_url = "https://api.themoviedb.org/3/movie/now_playing?&api_key=";
@@ -10,7 +12,7 @@ const search_movie_url = 'https://api.themoviedb.org/3/search/movie?api_key=';
 //const movieSearch = document.querySelector("#movieSearch");
 const submitButton = document.querySelector("#submitButton");
 const movieResults = document.querySelector(".movieResults");
-const movieSearch = document.querySelector("#movieSearch");
+const movieInput = document.querySelector("#movieSearch");
 const searchForm = document.querySelector("#searchForm");
 const movieDisplay = document.querySelector("#movie-display");
  //API URL
@@ -18,45 +20,54 @@ const movieDisplay = document.querySelector("#movie-display");
 
 // What we first see on the page
 async function firstPage(event){
+    event.preventDefault();
     const response = await fetch(search_movie_url + apiKey + "&query=" + event + "&page=" + page_num);
     const responseData = await response.json();
     const data = responseData.results
-    console.log("hey")
-    data.forEach(element => displayMovies(element));
+    //console.log("hey")git
+    data.forEach(element => displayMovie(element));
 
 }
+
+async function getResults(){
+    const api_url = now_playing_url + apiKey + "&q=" +  search_movie + "&language=en-US&page=" + page_num;
+    const response = await fetch(api_url);
+    const responseData = await response.json();
+    const data = responseData.results
+    data.forEach(element => displayMovie(element));
+}
 // This function is one that gets results from the API and it uses the fetch method with my custom HTTP request.
-async function getResults(event){
-    event.preventDefault();
-    try{
-        //var api_url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${movieSearch.value}&pages=1&include_adult=false`;
-        let response = await fetch(api_url);
+// // async function getResults(event){
+//     event.preventDefault();
+//     try{
+//         //var api_url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${movieSearch.value}&pages=1&include_adult=false`;
+//         let response = await fetch(api_url);
 
-        console.log("response is: ", response);
+//         console.log("response is: ", response);
 
-        let responseData = await response.json();
+//         let responseData = await response.json();
 
-        console.log("responseData is: ", responseData);
+//         console.log("responseData is: ", responseData);
 
-        displayMovies(responseData);
-    } catch (e) {
-        console.log();}
-    }
+//         displayMovies(responseData);
+//     } catch (e) {
+//         console.log();}
+//     }
 
     //fetch(api_url)
         //.then(responce => responce.json)
         //.then(data => console.log(data))
         
-window.onload = () => {
-    console.log("Hey");
-    //const searchMovies = document.querySelector("#searchMovies");
-    searchMovies.addEventListener("submit", getResults);
-   const movieSearch = document.querySelector("#movieSearch").value;
-    console.log(movieSearch);
-    }
+// window.onload = () => {
+//     console.log("Hey");
+//     //const searchMovies = document.querySelector("#searchMovies");
+//     searchMovies.addEventListener("submit", getResults);
+//    const movieSearch = document.querySelector("#movieSearch").value;
+//     console.log(movieSearch);
+//     }
 
 // Image is not displaying at all ?
-function displayMovies (event){
+function displayMovie (event){
     movieDisplay.innerHTML += `
     <div class ="movies"
         <img class="movie-poster" src = "https://image.tmdb.org/t/p/w500${event.poster_path} "alt=${event.title} />
@@ -71,3 +82,14 @@ function displayMovies (event){
             `
 }
 
+async function submit(event){
+    event.preventDefault();
+    movieDisplay.innerHTML = '';
+    search_movie = movieInput.value;
+    const results = await firstPage(search_movie);
+    displayMovie(results);
+    movieInput.value = '';
+}
+
+searchForm.addEventListener('submit', submit);
+getResults()
